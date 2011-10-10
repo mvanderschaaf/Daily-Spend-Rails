de9b6a2baa361da6936e721d6b0cbdbd040e6213
fadvansearch <mvdschaaf@gmail.com>
2011-10-10 04:10:45 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dailyspend::Application.config.secret_token = 'ebcd2f75b1d539cd7eb8d397aaf8f7349177f80c48b13fee66abe620e00b0cd4f8e38f9a3497a9f9c49ba8dd066c42df4a01a5576866b21e2d09cdb07317b322'
